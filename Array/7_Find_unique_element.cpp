60872458f77fed98f83cdadd3052495d22cc4ee0
raghvendra-official <raghvendraofficials@gmail.com>
2024-08-07 14:14:14 +0530

    Adding files

---
+++ Array/7_Find_unique_element.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+
+
+int main(){
+    int arr[9] = {2,10,11,13,10,2,15,13,15};
+    int size = 9;
+    int finalAns=getUnique(arr,size);
+    cout<<finalAns;
+    
+}
